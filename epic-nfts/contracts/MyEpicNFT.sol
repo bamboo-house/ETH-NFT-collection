8eb3c11be74e688ee4c79b7c38e2915dab98fd1f
bamboo-house <66010352+bamboo-house@users.noreply.github.com>
2022-10-17 13:25:30 +0900

    ローカルでイーサリアムネットワーク立ち上げ

---
+++ epic-nfts/contracts/MyEpicNFT.sol
@@ -0,0 +1,10 @@
+// MyEpicNFT.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+import "hardhat/console.sol";
+
+contract MyEpicNFT {
+  constructor() {
+    console.log("This is my NFT contract.");
+  }
+}
---
+++ epic-nfts/scripts/run.js
@@ -0,0 +1,21 @@
+const main = async () => {
+  // artifactsに必要なファイルを追加
+  const nftContractFactory = await hre.ethers.getContractFactory("MyEpicNFT");
+  // Hardhat がローカルの Ethereum ネットワークを作成する
+  const nftContract = await nftContractFactory.deploy();
+  // コントラクトが Mint され、ローカルのブロックチェーンにデプロイされるまで待つ
+  await nftContract.deployed();
+  console.log("Contract deployed to:", nftContract.address);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
